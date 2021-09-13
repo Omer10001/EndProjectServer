4d222c4d2a516c729e0c00fc937350ffa7fadc1b
User <User@LAB2-13>
2021-09-13 14:46:31 +0300

    Add project files.

---
+++ Style/Style/MainPageVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Style
+{
+    class MainPageVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPrpertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
