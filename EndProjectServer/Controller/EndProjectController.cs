fe0b29b7048684970b9a3de05d6ec0ae5adb79cd
User <User@LAB2-13>
2021-10-13 13:12:15 +0300

    server complete (for now)

--- EndProjectServer/Controller/EndProjectController.cs
+++ EndProjectServer/Controller/EndProjectController.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EndProjectServerBL.Models;
+using System.IO;
 
 namespace EndProjectServer.Controller
 {
@@ -11,5 +13,12 @@ namespace EndProjectServer.Controller
     [ApiController]
     public class EndProjectController : ControllerBase
     {
+        #region Add connection to the db context using dependency injection
+        EndProjectDBContext context;
+        public EndProjectController(EndProjectDBContext context)
+        {
+            this.context = context;
+        }
+        #endregion
     }
 }
