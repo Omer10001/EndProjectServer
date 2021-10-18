9f8824db07ff428b8a316db4b400cb377fa153d5
User <User@LAB2-13>
2021-10-18 14:09:44 +0300

    18.10

---
+++ EndProjectServerBL/ModelsBL/EndProjectDBContextBL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EndProjectServerBL.Models
+{
+  
+   
+        public partial class EndProjectDBContext : DbContext
+        {
+            public User Login(string email, string pswd)
+            {
+                 try
+                 {
+                     User user = this.Users
+                        .Where(u => u.Email == email && u.Password == pswd).FirstOrDefault();
+
+                     return user;
+                 }
+                 catch(Exception e)
+                 {
+                      throw new Exception("Could not Login. error retreiving Data", e);
+                 }
+               
+            }
+        }
+    
+}
