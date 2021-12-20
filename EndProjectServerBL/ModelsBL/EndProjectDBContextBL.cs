e8bd0cd32cd22b36168db14de9ea78b8fa3f5484
omer shalgi <oshalgi101@gmail.com>
2021-12-20 13:10:43 +0200

    a

--- EndProjectServerBL/ModelsBL/EndProjectDBContextBL.cs
+++ EndProjectServerBL/ModelsBL/EndProjectDBContextBL.cs
@@ -8,28 +8,28 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace EndProjectServerBL.Models
 {
-  
-   
-        public partial class EndProjectDBContext : DbContext
-        {
-            public User Login(string email, string pswd)
-            {
-                 try
-                 {
-                     User user = this.Users
-                        .Where(u => u.Email == email && u.Password == pswd).FirstOrDefault();
 
-                     return user;
-                 }
-                 catch(Exception e)
-                 {
-                      throw new Exception("Could not Login. error retreiving Data", e);
-                 }
-               
+
+    public partial class EndProjectDBContext : DbContext
+    {
+        public User Login(string email, string pswd)
+        {
+            try
+            {
+                User user = this.Users
+                   .Where(u => u.Email == email && u.Password == pswd).FirstOrDefault();
+
+                return user;
             }
+            catch (Exception e)
+            {
+                throw new Exception("Could not Login. error retreiving Data", e);
+            }
+
+        }
         public void CreateUser(User user)
         {
-           
+
             this.Users.Add(user);
             this.SaveChanges();
         }
